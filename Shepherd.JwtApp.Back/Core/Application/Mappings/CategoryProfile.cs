b0fda773260043fef388e4b73cba865b49006d03
Sinan Çoban <sinan.coban@outlook.com>
2023-09-09 11:20:26 +0300

    Added Map Profiles

---
+++ Shepherd.JwtApp.Back/Core/Application/Mappings/CategoryProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using Shepherd.JwtApp.Back.Core.Domain;
+
+namespace Shepherd.JwtApp.Back;
+public class CategoryProfile : Profile
+{
+    public CategoryProfile()
+    {
+        CreateMap<Category, CategoryListDto>().ReverseMap();
+    }
+}
---
+++ Shepherd.JwtApp.Back/Core/Application/Mappings/ProductProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using Shepherd.JwtApp.Back.Core.Domain;
+
+namespace Shepherd.JwtApp.Back;
+public class ProductProfile : Profile
+{
+    public ProductProfile()
+    {
+        CreateMap<Product,ProductListDto>().ReverseMap();
+    }
+}
