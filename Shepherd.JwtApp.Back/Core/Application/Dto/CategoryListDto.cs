8dec87a8d29c826032dd88e25e58e4932f056ca9
Sinan Çoban <sinan.coban@outlook.com>
2023-09-09 11:17:52 +0300

    Added Dto

---
+++ Shepherd.JwtApp.Back/Core/Application/Dto/CategoryListDto.cs
@@ -0,0 +1,6 @@
+﻿namespace Shepherd.JwtApp.Back;
+public class CategoryListDto
+{
+    public int Id {get; set;}
+    public string? Definition {get; set;}
+}
---
+++ Shepherd.JwtApp.Back/Core/Application/Dto/ProductListDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Shepherd.JwtApp.Back;
+public class ProductListDto
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int Stock { get; set; }
+    public decimal Price { get; set; }
+    public int CategoryId { get; set; }
+    
+}
