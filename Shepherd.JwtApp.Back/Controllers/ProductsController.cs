d044fcb1f3de5610a6701ebd4d2dbe24d387b6f7
Sinan Çoban <sinan.coban@outlook.com>
2023-09-09 11:18:18 +0300

    Added Controller

---
+++ Shepherd.JwtApp.Back/Controllers/ProductsController.cs
@@ -0,0 +1,49 @@
+﻿
+
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Shepherd.JwtApp.Back.Core.Domain;
+
+namespace Shepherd.JwtApp.Back;
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ProductsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    [HttpGet]
+    public async Task<IActionResult> List(){
+        var result = await _mediator.Send(new GetAllProductsQueryRequest());
+        return Ok(result);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(int id)
+    {
+        var result = await _mediator.Send(new GetProductQueryRequest(id));
+        return result == null ? NotFound() : Ok(result);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _mediator.Send(new DeleteProductCommandRequest(id));
+        return NoContent();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateProductCommandRequest request)
+    {
+        await _mediator.Send(request);
+        return Created("",request);
+    }
+    [HttpPut]
+    public async Task<IActionResult> Update(UpdateProductCommandRequest request)
+    {
+        await _mediator.Send(request);
+        return NoContent();
+    }
+}
